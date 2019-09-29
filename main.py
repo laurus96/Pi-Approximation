2fbf517374d1e03a434b49249428527d039ff8ac
Lorenzo Paolo Cocchinone <lorenzo.cocchinone@gmail.com>
2019-09-29 13:08:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,52 @@
 import itertools as it
 import sys
 import signal
-import math
-import time
+from time import sleep
+from os import system, name
 
+pi = float(0.0)
 
-def signal_handler(sig, frame, pi_value=3.14):
+def signal_handler(sig, frame):
     """This function ask if the user want to continue to approximate PI"""
+
+    clear()
     print('You pressed Ctrl+C')
     answer = str(input('Do you want to stop calculate Pi? [yes/no]'))
     
     if answer == 'no':
         return
     else:
-        print(pi_value)
+        print(pi)
         sys.exit(0)
 
-    #main 
+def clear():
+    """ This fuction clear the terminal (windows, macosx adn linux)"""
+    
+    if name == 'nt':
+        _ = system('cls')
+    else:
+        _ = system('clear')
+
+#main 
 
 if __name__ == '__main__':
-
+    
     den = float(0.0)
     num = int(0)
     pi_app = float(0.0)
     
 
     for n in it.count():
+        
         den = (2 * n) + 1
         num = (-1) ** n
 
+        pi_app = pi_app + (num / den) #this calculate (pi/4)
 
-        pi_app = pi_app + (num / den)
-        print(pi_app*4)
-        time.sleep(2)
+        pi = pi_app * 4
+        print(pi)
+        sleep(0.6)
+        clear()
 
         signal.signal(signal.SIGINT, signal_handler)
 
