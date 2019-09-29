d36a257a1a8c2fa1670319e240b420bf4fc618da
Lorenzo Paolo Cocchinone <lorenzo.cocchinone@gmail.com>
2019-09-29 12:46:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import itertools as it
 import sys
 import signal
 import math
+import time
+
 
 def signal_handler(sig, frame, pi_value=3.14):
     """This function ask if the user want to continue to approximate PI"""
@@ -19,10 +21,19 @@ def signal_handler(sig, frame, pi_value=3.14):
 if __name__ == '__main__':
 
     den = float(0.0)
+    num = int(0)
+    pi_app = float(0.0)
+    
 
-    for x in it.count():
-        print(x)
-        
+    for n in it.count():
+        den = (2 * n) + 1
+        num = (-1) ** n
+
+
+        pi_app = pi_app + (num / den)
+        print(pi_app*4)
+        time.sleep(2)
+
         signal.signal(signal.SIGINT, signal_handler)
 
 
