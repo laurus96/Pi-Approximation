bcec72c6b445da35a9aa4bec21d9637a5c57af61
Lorenzo Paolo Cocchinone <lorenzo.cocchinone@gmail.com>
2019-09-29 14:11:42 +0200

    Create ui.py
    File for test PyQT5

---
+++ ui.py
@@ -0,0 +1,11 @@
+from PyQt5.QtWidgets import QApplication, QLabel, QVBoxLayout, QPushButton, QWidget
+
+app = QApplication([])
+app.setStyle('Macintosh')
+label = QLabel('Hello world!')
+window = QWidget()
+layout = QVBoxLayout()
+layout.addWidget(label)
+window.setLayout(layout)
+window.show()
+app.exec_()
