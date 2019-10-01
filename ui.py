531a3867c7b05bf85133a794c40aa68c38f95635
Lorenzo Paolo Cocchinone <lorenzo.cocchinone@gmail.com>
2019-10-01 12:14:04 +0200

    implement for visual studio code

--- ui.py
+++ ui.py
@@ -1,14 +1,11 @@
-from PyQt5.QtWidgets import QApplication, QLabel, QVBoxLayout, QPushButton, QWidget
+from PyQt5 import QtWidgets
 
-app = QApplication([])
-app.setStyle('Macintosh')
-label = QLabel('Hello world!')
-window = QWidget()
-layout = QVBoxLayout()
+app = QtWidgets.QApplication([])
+label = QtWidgets.QLabel('Ciao Mondo!')
+window = QtWidgets.QWidget()
+layout = QtWidgets.QVBoxLayout()
 layout.addWidget(label)
 window.setLayout(layout)
+window.setWindowTitle("Test")
 window.show()
 app.exec_()
-    
-
-
